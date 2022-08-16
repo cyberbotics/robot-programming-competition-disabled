d0eafd78ff538b4dab97addc2b7b3640be4b7829
Thomas Kimble <100125668+ThomasOliverKimble@users.noreply.github.com>
2022-08-16 16:42:49 +0200

    Update supervisor.py

--- controllers/supervisor/supervisor.py
+++ controllers/supervisor/supervisor.py
@@ -8,18 +8,14 @@ def benchmarkPerformance(message, robot):
     benchmarkName = message.split(':')[1]
     benchmarkPerformance = message.split(':')[2]
     benchmarkPerformanceString = message.split(':')[3]
-    print(benchmarkName + ' Benchmark complete! Your performance was ' + benchmarkPerformanceString);
-    stop_recording(robot)
+    print(benchmarkName + ' Benchmark complete! Your performance was ' + benchmarkPerformanceString)
+    if robot.getFromDef("ANIMATION_RECORDER_SUPERVISOR"):
+        stop_recording(robot)
 
 def stop_recording(robot):
-  if robot.getFromDef("ANIMATION_RECORDER_SUPERVISOR"):
-      for _ in range(50):
-          robot.step(timestep)
-      robot.animationStopRecording()
-      print('Benchmark completed. The animation is saved.')
-      for _ in range(10):
-          robot.step(timestep)
-      robot.simulationQuit(0)
+    emitter = robot.getDevice('emitter')
+    emitter.send("done".encode('utf-8'))
+    robot.getFromDef("BENCHMARK_SUPERVISOR").restartController()
 
 """ try:
     includePath = "../../../include"
@@ -49,7 +45,7 @@ while robot.step(timestep) != -1:
         if t[0] < -0.01 and abs(t[0] - tx) < 0.0001:  # away from starting position and not moving any more
             running = False
             name = 'Robot Programming'
-            performance = percent
+            performance = str(percent)
             performanceString = str(round(percent * 100, 2)) + '%'
             message = 'success:' + name + ':' + performance + ':' + performanceString
             benchmarkPerformance(message, robot)
@@ -58,12 +54,12 @@ while robot.step(timestep) != -1:
         message += ":" + str(percent)
         robot.wwiSendText(message)
         tx = t[0]
-    else:  # wait for record message
+"""     else:  # wait for record message
         message = robot.wwiReceiveText()
         while message:
             if message.startswith("success:"):
                 benchmarkPerformance(message, robot)
                 break
-            message = robot.wwiReceiveText()
+            message = robot.wwiReceiveText() """
 
 robot.simulationSetMode(Supervisor.SIMULATION_MODE_PAUSE)
